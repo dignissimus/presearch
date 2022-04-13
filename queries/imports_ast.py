7314d5591f1b9e7ad23a48d79da621a1b7c0f37c
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-04-13 23:50:11 +0100

    Reformat
    Signed-off-by: Sam Ezeh <sam.z.ezeh@gmail.com>

--- queries/imports_ast.py
+++ queries/imports_ast.py
@@ -1,9 +1,8 @@
 from presearch.query import MatchQuery
 
+
 def module_imports_ast(module):
     return module.imports("ast")
- 
-query = MatchQuery(
-    module_imports_ast
-)
 
+
+query = MatchQuery(module_imports_ast)
